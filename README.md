d28c635aa4e0d27a7562a221e34edd8e3a0c67a4
Tanmay Vij <tanmayvij047695@gmail.com>
2019-05-01 05:43:09 +0530

    Initial Commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# Mean Chat App
+
+Basic chatroom application developed using MEAN Stack
+
+## Requirements
+
+> node v8+
+
+> npm v5+
+
+> Angular CLI
+
+## Install and Run
+
+> npm i
+
+> npm run build
+
+> npm start
---
+++ src/app/app.component.ts
@@ -0,0 +1,57 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+   loginForm: FormGroup;
+  messageForm: FormGroup;
+  messages: any = [];
+  
+  constructor(private formbuilder: FormBuilder, private http: HttpClient) {
+    this.loginForm = this.formbuilder.group({
+      'username': ['', Validators.required]
+    });
+    this.messageForm = this.formbuilder.group({
+      'message': ['', Validators.required]
+    });
+  }
+  ngOnInit() {
+    this.getMessages();
+  }
+  loggedIn()
+  {
+    return sessionStorage.username ? true : false;
+  }
+  login()
+  {
+    sessionStorage.setItem('username', this.loginForm.controls.username.value);
+    this.getMessages();
+  }
+  getMessages()
+  {
+    this.http.get("/api/messages").subscribe(resp => {
+      this.messages = resp;
+    });
+  }
+  postMessage()
+  {
+    let postObj : any = {
+      "username": sessionStorage.username,
+      "message": this.messageForm.controls.message.value
+    }
+    let httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json'
+        })
+    };
+    this.http.post("/api/messages", postObj, httpOptions).subscribe(resp => {
+      this.getMessages();
+    });
+    this.messageForm.reset();
+  }
+}
